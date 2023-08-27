b47a635ac3a0f1e46806b0e2195db575361ebf0b
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-08-27 12:53:43 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio2-Ejercicio9-consola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.WriteLine("Ingrese los números");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 >num2 && num1 > num3)
+{
+    Console.WriteLine("El número mayor es "+num1);
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("El número mayor es "+ num2);
+}
+else Console.WriteLine("El número mayor es "+ num3);
+
